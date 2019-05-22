4e6c6862d1e8649fb4257bff7b4b76fd11cd5b78
Winston Chen <weijiechen1994@gmail.com>
2019-05-22 14:27:40 -0500

    Add files via upload
    Install the package with a one-line command

---
+++ installation.sh
@@ -0,0 +1,5 @@
+conda install -c anaconda -y tensorflow-gpu keras-gpu nomkl
+conda install -c conda-forge -y pillow opencv nibabel qimage2ndarray nilearn pytables
+conda install -c simpleitk -y simpleitk
+pip install git+https://www.github.com/farizrahman4u/keras-contrib.git
+
